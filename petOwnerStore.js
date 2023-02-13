153b85290f822bd424f6a30f72216044bd2be546
Ali-consciousroad <ali_dindar@live.be>
2023-02-13 17:43:33 +0100

    Basic representation of the store

---
+++ petOwnerStore.js
@@ -0,0 +1,79 @@
+class PetOwnerStore {
+    pets = [];
+    owners = [];
+
+    // GREANT ACCESS FOR GET 
+
+    // Total number owners 
+    get totalOwners() {
+        return this.owners.length;
+    }
+
+    // Total number pets
+    get totalPets() {
+        return this.pets.length;
+    }
+
+    // Get pets using ownerId
+    getPetsByOwnerId(ownerId) {
+        return this.pets.filter((pet) => {
+            return pet.owner && pet.owner.id === ownerId;
+        });
+    }
+
+    // AUTOMATICALLY UPDATE ITEMS
+    // Accept a pet object and add it to the pets array of the current instance
+    createPet(pet = { id: 0, name: '', type: '', breed: '', owner: null }) {
+        this.pets.push(pet);
+    }
+
+    // Accept an owner object and add it to the owners array
+    createOwner(owner = { id: 0, firstName: '', lastName: '' }){
+        this.owners.push(owner);
+    }
+
+    // Update owner
+    updateOwner(ownerId, update) {
+        const ownerIndexAtId = this.owners.findIndex((owner) => owner.id === ownerId);
+        if (ownerIndexAtId > -1 && update) {
+            this.owners[ownerIndexAtId] = update;
+        }
+    }
+
+    updatePet(petId, update) {
+        const petIndexAtId = this.pets.findIndex((pet) => pet.id === petId);
+        if (petIndexAtId > -1 && update) {
+            this.pets[petIndexAtId] = update;
+        }
+    }
+
+    // REMOVE ITEMS 
+    // delete pet by user id
+    deletePet(petId) {
+        const petIndexAtId = this.pets.findIndex((pet) => pet.id === petId);
+        if (petIndexAtId > -1) {
+            this.pets.splice(petIndexAtId, 1);
+        }
+    }
+
+    // delete owner by owner id
+    deleteOwner(ownerId) {
+        const ownerIndexAtId = this.owners.findIndex((owner) => owner.id === ownerId);
+        if (ownerIndexAtId > -1) {
+            this.owners.splice(ownerIndexAtId, 1);
+        }
+    }
+
+    // ASSIGN id 
+    assignOwnerToPet(ownerId, petId) {
+        const petIndexAtId = this.pets.findIndex((pet) => pet.id === petId);
+        const ownerIndexAtId = this.owners.findIndex((pet) => pet.id === ownerId);
+        if (petIndexAtId > -1 && ownerIndexAtId > -1) {
+            this.pets[petIndexAtId].owner = this.owners[petIndexAtId];
+        }
+    }
+
+    // get store details
+    get storeDetails() {
+        return `We have ${this.totalPets()} total pets and ${this.totalOwners()} total owners, so far!!!`;
+}
