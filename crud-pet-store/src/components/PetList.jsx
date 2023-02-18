71a6712e9b1e5cdd7884adc625b1e0ac6af24036
Ali-consciousroad <ali_dindar@live.be>
2023-02-18 16:12:39 +0100

    Adapting components to allow interactions and add pet details

--- crud-pet-store/src/components/PetList.jsx
+++ crud-pet-store/src/components/PetList.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 function PetList({ store }) {
-    function PetList({ store}) {
-        return <div>{store.storeDetails}</div>;
-    }
+  // Event handler to add a new pet
+  const handleAddPet = () => {
+    const name = prompt("Enter pet name");
+    const type = prompt("Enter pet type");
+    const breed = prompt("Enter pet breed");
+    const ownerId = prompt("Enter owner's Id of the pet");
+
+    const pet = store.createPet({ id: Date.now(), name, type, breed });
+    store.assignOwnerToPet(ownerId, pet.id);
+  };
+  return (
+    <div>
+      {store.storeDetails}
+      <button onClick={handleAddPet}>Add Pet</button>
+    </div>
+  );
 }
 
 export default PetList;
