9a2c68d0e74d3bbbaa7c5364bd669f2a40d5cf8f
Ali-consciousroad <ali_dindar@live.be>
2023-03-26 15:35:17 +0200

    Create the CRUD functionality for owners

--- crud-pet-store/src/App.jsx
+++ crud-pet-store/src/App.jsx
@@ -1,11 +1,17 @@
 import PetOwnerStore from "./PetOwnerStore";
 import PetList from "./components/PetList";
+import OwnerList from "./components/OwnerList";
+import "./App.css";
 
 function App() {
   const store = new PetOwnerStore();
   return (
     <div className="App">
+      <h3>Pets List</h3>
       <PetList store={store} />
+      <hr />
+      <h3>Owners List</h3>
+      <OwnerList store={store} />
     </div>
   );
 }
